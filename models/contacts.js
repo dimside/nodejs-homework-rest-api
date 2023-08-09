4ee6b8033a1e96711ae423e81c1f29514b9035ee
dimside <dimside29@gmail.com>
2023-08-09 03:03:59 +0300

    completed-hw02-express

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,50 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
+const crypto = require("crypto");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "contacts.json");
 
-const getContactById = async (contactId) => {}
+async function writeContacts(contacts) {
+  return await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+}
 
-const removeContact = async (contactId) => {}
+const listContacts = async () => {
+  const contacts = await fs.readFile(contactsPath, "utf-8");
+  return JSON.parse(contacts);
+};
 
-const addContact = async (body) => {}
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  return contacts.find(({ id }) => id === contactId) || null;
+};
 
-const updateContact = async (contactId, body) => {}
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const contactIndex = contacts.findIndex(({ id }) => id === contactId);
+  if (contactIndex === -1) return null;
+  const newContacts = contacts.filter(({ id }) => id !== contactId);
+  const deletedContact = contacts[contactIndex];
+  writeContacts(newContacts);
+  return deletedContact;
+};
+
+const addContact = async ({ name, email, phone }) => {
+  const contacts = await listContacts();
+  const contact = { name, email, phone, id: crypto.randomUUID() };
+  contacts.push(contact);
+  writeContacts(contacts);
+  return contact;
+};
+
+const updateContact = async (contactId, body) => {
+  const contacts = await listContacts();
+  const contactIndex = contacts.findIndex(({ id }) => id === contactId);
+  if (contactIndex === -1) return null;
+  const newContact = { ...contacts[contactIndex], ...body };
+  contacts[contactIndex] = newContact;
+  writeContacts(contacts);
+  return newContact;
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +52,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
